a538232fa378718b7e2d6e77fb0f27016107c991
Petr Dytrych <petrdytrych@gmail.com>
2011-04-23 11:02:35 +0200

    Added missing presneters - WikiPresenter, WidgetPresenter

--- src/com/jwl/presentation/enumerations/JWLPresenters.java
+++ src/com/jwl/presentation/enumerations/JWLPresenters.java
@@ -4,12 +4,16 @@ import com.jwl.presentation.components.administration.AdministrationPresenter;
 import com.jwl.presentation.components.article.ArticlePresenter;
 import com.jwl.presentation.components.core.AbstractPresenter;
 import com.jwl.presentation.components.search.SearchPresenter;
+import com.jwl.presentation.components.widget.WidgetPresenter;
+import com.jwl.presentation.components.wiki.WikiPresenter;
 
 public enum JWLPresenters {
 
 	ARTICLE("article", ArticlePresenter.class),
 	ADMINISTRATION("administration", AdministrationPresenter.class),
-	SEARCH("search", SearchPresenter.class);
+	SEARCH("search", SearchPresenter.class),
+	WIKI("wiki", WikiPresenter.class),
+	WIDGET("widget", WidgetPresenter.class);
 	
 	public final String id;
 	public final Class<? extends AbstractPresenter> clazz;
