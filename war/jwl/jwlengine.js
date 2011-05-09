431cd439e80dd4bdf2cfcc611f4a1fb2d74626a8
Lukas Rychtecky <lukas.rychtecky@antee.cz>
2011-05-09 11:06:56 +0200

    Fix nested component given thru AJAX.

--- war/jwl/jwlengine.js
+++ war/jwl/jwlengine.js
@@ -14,7 +14,7 @@ var JWL = {
 			{
 				url: "ajax.seam?jwlmethod=ajax&jwluri=" + uri + href,
 				success: function(data) {
-					$(thisTrigger).parents('.jwl-component').html(data);
+					$(thisTrigger).parents('.jwl-component').html($(data).html());
 					JWL.markIt();
 					starRating.create('.jwl-view-stars');
 					knowledgeAjax.init();
